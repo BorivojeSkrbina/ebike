a032d1b3f4fb2cd520147b0eb7da3a5bf2c82ad0
Borivoje Skrbina <borivoje.s@gmail.com>
2017-12-10 23:43:55 +0100

    prikaz baterije, jos zeza css

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ $(document).ready(function () {
         var voltage = (series * 3.6).toFixed(1);
         var capacity = (cellcapacity * parallel).toFixed(1);
         var power = (voltage * capacity).toFixed(0);
+        
+        
         $('h1.battery').text('Battery: ' + voltage + 'V, ' + series + 's' + parallel + 'p, ' + config + ' cells');
         $('h1.capacity').text('Capacity: ' + capacity + "Ah");
         $('h1.power').text('Power: ' + power + "Wh");
@@ -21,17 +23,19 @@ $(document).ready(function () {
 //PRIKAZ BATERIJE
         var i;
         var a;
-        $('.battery-row ul li').empty();
+        $('.battery-row ul li + li').remove();
+        $('.battery-row ul li img').remove();
+
 
         for (i = 1; i <= series; i++) {
             if (i % 2 === 0) {
-                $('.battery-row ul li').append('<img src="img/batnegative.png" class="d-inline-block">' + " ");
+                $('.battery-row ul li').append('<img src="img/batnegative.png" class="d-inline-block">');
             } else {
-                $('.battery-row ul li').append('<img src="img/batpositive.png" class="d-inline-block">' + " ");
+                $('.battery-row ul li').append('<img src="img/batpositive.png" class="d-inline-block">');
             }
         }
 
-        var seriesModule = $('.battery-row ul').html();
+        var seriesModule = $('.battery-row ul').first().html();
 
         for (a = 1; a < parallel; a++) {
             $('.battery-row ul').append(seriesModule);
@@ -40,24 +44,24 @@ $(document).ready(function () {
 
         var r;
         
-        $('.battery-row ul li').css({"top": "0","left": "0"});
-        var top = 25;
-        var left = 10;
+        var top = 21;
+        var left = 15;
         for (r = 1; r <= parallel; r++) {
             $('.battery-row ul li').eq(r).css({"top": top,"left": left});
-            top += 25;
-            left += 10;
+            top += 21;
+            left += 15;
             console.log(r);
 
         }
 
 
+//        alert('.battery-row ul li').html();
 
 
     });//BUTTON CLICK END
 
 
-    //$('.battery-row li + li').
+    
 
 
 
