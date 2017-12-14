ab58b67c074f18a45e31d3fdc713aeb5b0a21e6f
Borivoje Skrbina <borivoje.s@gmail.com>
2017-12-14 11:59:25 +0100

    button prevent default zeza validaciju

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $('.izracunaj').click(function (e) {
+    $('.configure').click(function (e) {
         e.preventDefault();
         var series = parseInt($('input[name="series"]').val());
         var parallel = parseFloat($('input[name="parallel"]').val());
@@ -12,19 +12,20 @@ $(document).ready(function () {
         var voltage = (series * 3.6).toFixed(1);
         var capacity = (cellcapacity * parallel).toFixed(1);
         var power = (voltage * capacity).toFixed(0);
-        
-        
+
+
         $('h1.battery').text('Battery: ' + voltage + 'V, ' + series + 's' + parallel + 'p, ' + config + ' cells');
         $('h1.capacity').text('Capacity: ' + capacity + "Ah");
         $('h1.power').text('Power: ' + power + "Wh");
         $('h1.maxcurrent').text('Max current: ' + cellcurrent * parallel + "A");
-console.time("factorial test");
+        console.time("factorial test");
 
 //PRIKAZ BATERIJE
         var i;
         var a;
-        $('.battery-row ul li + li').remove();
-        $('.battery-row ul li img').remove();
+        //$('.battery-row ul li + li').remove();
+        //$('.battery-row ul li img').remove();
+        $('.battery-row ul').empty().append('<li class="d-flex"></li>');
 
 
         for (i = 1; i <= series; i++) {
@@ -47,17 +48,17 @@ console.time("factorial test");
         var top = 21;
         var left = 15;
         for (r = 1; r < parallel; r++) {
-            $('.battery-row ul li').eq(r).css({"top": top,"left": left});
+            $('.battery-row ul li').eq(r).css({"top": top, "left": left});
             top += 21;
             left += 15;
         }
 
-console.timeEnd("factorial test");
+        console.timeEnd("factorial test");
 
     });//BUTTON CLICK END
 
 
-    
+
 
 
 
